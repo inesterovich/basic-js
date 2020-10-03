62d6e6dd8e0cb1d85632594a015cae2ab61119f4
Ilya Nesterovich <manager@inesterovich.ru>
2020-10-03 13:30:45 +0300

    feat: add other tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+   if (Array.isArray(arr)) {
+     return 1 + arr.reduce(
+      (prevResult, curItem) => Math.max(this.calculateDepth(curItem), prevResult), 0
+     )
+     
+     ;
+   } else {
+     return 0;
+   }
+
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,97 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor(boolean) {
-    if (boolean === false) {
-      this.type = 'reverse';
-    } else {
-      this.type = 'direct';
-    }
-
+  constructor(data = true) {
+    this.data = data;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-
   encrypt(message, key) {
-    while (key.length < message.length) {
-      key += key;
-    }
-
-    let messageArray = message.toUpperCase().split('');
-    let keyArray = key.toUpperCase().split('');
-
-    for (let i = 0; i < messageArray.length; i++) {
-      if (messageArray[i].charCodeAt(0) < 65) {
-        keyArray.splice(i, 0, messageArray[i]);
-      }
-
-    }
-
-
-    let messageCode = messageArray.map(x => x.charCodeAt(0));
-    let keyCode = keyArray.slice(0, message.length).map(x => x.charCodeAt(0) - 65);// Косяк вот тут. Вычитать только при определенном условии
-    let encryptArray = [];
-
-    for (let i = 0; i < messageCode.length; i++) {
-
-      if (messageCode[i] < 65 || messageCode[i] > 90) {
-        encryptArray.push(messageCode[i]);
-
+    if(!message || !key) throw new Error();
+    const maxLength = message.length;
+    let resultArr = [];
+    let step = -1;
+    for (let i = 0; i < maxLength; i++) {
+      if (this.alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        step++;
+        let num = (this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.indexOf(key[step % key.length].toUpperCase())) % this.alphabet.length;
+        resultArr = [...resultArr, ...this.alphabet[num]];
       } else {
-        encryptArray.push(messageCode[i] + keyCode[i])
+        resultArr = [...resultArr, ...message[i]];
       }
-
-      if (encryptArray[i] > 90) {
-        encryptArray[i] -= 26;
-      }
-
-      // Где-то тут что-то не то. Пробелы превращаются в черти что
-
     }
-
-    let result = encryptArray.map(x => String.fromCharCode(x));
-
-    return result.join('');
-
-
-
+    return this.data ? resultArr.join('') : resultArr.reverse().join('');
   }
+
   decrypt(message, key) {
-    while (key.length < message.length) {
-      key += key;
-    }
-
-    let messageArray = message.toUpperCase().split('');
-    let keyArray = key.toUpperCase().split('');
-
-    for (let i = 0; i < messageArray.length; i++) {
-      if (messageArray[i].charCodeAt(0) < 65 || messageArray[i] > 90) {
-        keyArray.splice(i, 0, messageArray[i]);
-      }
-
-    }
-
-    let messageCode = messageArray.map(x => x.charCodeAt(0));
-    let keyCode = keyArray.slice(0, message.length).map(x => 65 <= x.charCodeAt(0) <= 90 ? x.charCodeAt(0) - 65 : x.charCodeAt(0));
-    let decryptedArray = [];
-    for (let i = 0; i < messageCode.length; i++) {
-      if (messageCode[i] < 65 || messageCode[i] > 90) {
-        decryptedArray.push(messageCode[i]);
-        continue;
+    if(!message || !key) throw new Error();
+    const maxLength = message.length;
+    let resultArr = [];
+    let step = -1;
+    for (let i = 0; i < maxLength; i++) {
+      if (this.alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        step++;
+        let num = (this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.length - this.alphabet.indexOf(key[step % key.length].toUpperCase())) % this.alphabet.length;
+        resultArr = [...resultArr, ...this.alphabet[num]];
       } else {
-        decryptedArray.push(messageCode[i] - keyCode[i])
+        resultArr = [...resultArr, ...message[i]];
       }
-
-      if (decryptedArray[i] < 65) {
-        decryptedArray[i] += 26
-      }
-
-
     }
-
-    let result = decryptedArray.map(x => String.fromCharCode(x));
-    return result.join('');
-
-
+    return this.data ? resultArr.join('') : resultArr.reverse().join('');
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
